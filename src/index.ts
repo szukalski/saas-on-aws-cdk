bb7a0d075af2478be5c0e02016793356f94d6404
Dave Roberts <szukalski@gmail.com>
2025-06-04 20:12:20 +0000

    cognito

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { SoaLogGroup } from './log-group/log-group';
 export { SoaTableV2 } from './data/dynamodb';
+export { SoaUserPool } from './identity/cognito'
+export { nextId } from './util/id'
