558b36e767b3eae90893967b6d4cda3dbb147ab0
Dave Roberts <szukalski@gmail.com>
2025-06-24 08:54:57 +0000

    update

--- src/smithy/util-smithy.ts
+++ src/smithy/util-smithy.ts
@@ -1,5 +1,5 @@
 import { AwsCredentialIdentity } from '../token-vending-machine/util-token-vending-machine';
 
 export interface SmithyContext {
-  readonly credentials: AwsCredentialIdentity;
+  readonly credentials?: AwsCredentialIdentity;
 }
